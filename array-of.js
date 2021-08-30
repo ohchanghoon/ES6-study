9f82594f7cbef5d30233bb0c7777d7cada6a0736
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-08-30 23:01:07 +0900

    Array.of() 문법정리

---
+++ array-of.js
@@ -0,0 +1,8 @@
+// Array.of()
+// 전달받은 모든 인수로 배열을 생성한다
+
+const digits = Array.of(1, 2, 3, 4, 5);
+console.log(digits); // [1, 2, 3, 4, 5]
+
+const str = Array.of('1', '2', 'abc', '😀');
+console.log(str); // [ '1', '2', 'abc', '😀' ]
