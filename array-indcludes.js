97278344a8cb9bcf157830f99ca1674ea0d09df8
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-09-11 21:42:57 +0900

    Array.prototype.includes 문법정리

---
+++ array-indcludes.js
@@ -0,0 +1,6 @@
+// Array.prototype.includes()
+// 배열에 특정 원소가 포함되어 있으면 true를 반환하고 그렇지 않으면 false를 반환
+
+let arr = [1, 2, 4, 5];
+console.log(arr.includes(2)); // true
+console.log(arr.includes(3)); // false
